b874fff8ae99ce53f8552be0593fb5f1c2f9e088
Asher Gomez <ashersaupingomez@gmail.com>
2026-01-09 16:58:20 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # spoke
-Minimal client for the Spoke REST API with types
+
+Minimal client for the Spoke REST API with types.
+
+## Usage
+
+```ts
+import { createSpokeClient } from "@iuioiua/spoke";
+
+const spokeClient = createSpokeClient("your_spoke_api_key");
+
+const { data } = await spokeClient.GET("/plans");
+
+// ...
+```
---
+++ mod.ts
@@ -0,0 +1,34 @@
+import createClient from "openapi-fetch";
+import type { paths } from "./types.d.ts";
+
+/**
+ * Create a Spoke REST API client.
+ *
+ * @see {@link https://developer.dispatch.spoke.com | Spoke API Documentation}
+ * for endpoints and usage details.
+ *
+ * @example Usage
+ * ```ts
+ * import { createSpokeClient } from "@iuioiua/spoke";
+ *
+ * const spokeClient = createSpokeClient("your_spoke_api_key");
+ *
+ * const { data } = await spokeClient.GET("/plans");
+ *
+ * // ...
+ * ```
+ *
+ * @param apiKey Spoke REST API key. Generate one at {@link https://dispatch.spoke.com/settings/integrations}.
+ * @returns Spoke REST API client
+ */
+export function createSpokeClient(
+  apiKey: string,
+): ReturnType<typeof createClient<paths>> {
+  return createClient<paths>({
+    baseUrl: "https://api.getcircuit.com/public/v0.2b",
+    fetch: (request) => {
+      request.headers.set("Authorization", `Bearer ${apiKey}`);
+      return fetch(request);
+    },
+  });
+}
