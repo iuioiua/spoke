247d23c1584b477fafc4e08a4df8f1b2507b7b59
Asher Gomez <ashersaupingomez@gmail.com>
2026-02-04 14:54:40 +1100

    undo tests for now

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "@std/assert";
 import { stub } from "@std/testing/mock";
-import { createRateLimitMiddleware, createSpokeClient } from "./mod.ts";
+import { createSpokeClient } from "./mod.ts";
 
 Deno.test("createSpokeClient()", async () => {
   const apiKey = "test-api-key";
@@ -26,25 +26,3 @@ Deno.test("createSpokeClient()", async () => {
   assertEquals(authorizationHeader, `Bearer ${apiKey}`);
   assertEquals(requestUrl, `${baseUrl}/plans`);
 });
-
-// Deno.test("createRateLimitMiddleware()", async () => {
-//   const middleware = createRateLimitMiddleware();
-//   const baseUrl = "https://api.getcircuit.com/public/v0.2b";
-//   let authorizationHeader: string | null = null;
-//   let requestUrl: string | null = null;
-
-//   using _fetchStub = stub(globalThis, "fetch", (input, init) => {
-//     const request = new Request(input, init as RequestInit);
-//     authorizationHeader = request.headers.get("Authorization");
-//     requestUrl = request.url;
-//     return Promise.resolve(
-//       new Response("{}", {
-//         status: 200,
-//         headers: { "content-type": "application/json" },
-//       }),
-//     );
-//   });
-
-//   const client = createSpokeClient("test-api-key");
-//   await client.GET("/plans");
-// });
